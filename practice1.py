12e15c65f0b99b49e06345719fbf95fd3a0375c1
mohaddese <210817389+mohaddese-zamani@users.noreply.github.com>
2025-07-15 13:01:46 +0330

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -2,6 +2,10 @@ def practice1(sentence):
     yekta = []
     tartib_tekrar = {}
 
-    for i in sentence:
+    # for i in sentence:
         
+
+    if counter == 1:
+        yekta.append(i)
+    reaturn yekta
 practice1()
