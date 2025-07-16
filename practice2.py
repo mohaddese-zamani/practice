f1d226a9d3ba068fe466937e3576a35ee4bbd853
mohaddese-zamani <mohaddeseh651@gmail.com>
2025-07-16 15:52:31 +0330

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -6,14 +6,15 @@ def title_json():
     open_file = os.path.join(os.path.expanduser("~"), "Desktop", "home-page.txt")
     if not os.path.exists(open_file):
         print("فایل مورد نظر پیدا نشد:", open_file)
-    else:
-        try:
-            with open(open_file, "r", encoding="utf-8") as file:
-                reade_file = json.load(file)
+    
+    try:
+        with open(open_file, "r", encoding="utf-8") as file:
+            reade_file = json.load(file)
 
-            for title_file in reade_file.get("posts", []):
-                print(title_file.get("title"))
-        except json.JSONDecodeError:
-            print("خطا در خواندن فایل JSON")
+        for title_file in reade_file.get("posts", []):
+            print(title_file.get("title"))
+    except json.JSONDecodeError:
+        print("خطا در خواندن فایل JSON")
 
+print("عنوان های فایل\n")
 title_json()
