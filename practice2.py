25fcd9d138c59e83e71bd8820c4ab5aeaa1eaca0
mohaddese <210817389+mohaddese-zamani@users.noreply.github.com>
2025-07-18 00:00:03 +0330

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,20 +1,43 @@
 import json
 import os
 
-def title_json():
+json_file = os.path.join(os.path.expanduser("~"), "Desktop", "home-page.txt")
+read_file = {}
 
-    open_file = os.path.join(os.path.expanduser("~"), "Desktop", "home-page.txt")
-    if not os.path.exists(open_file):
-        print("فایل مورد نظر پیدا نشد:", open_file)
-    
+def open_json():
+    if not os.path.exists(json_file):
+        print("فایل مورد نظر پیدا نشد:", json_file)
+        return False
+
+def read_json():
+    global read_file
     try:
-        with open(open_file, "r", encoding="utf-8") as file:
-            reade_file = json.load(file)
-
-        for title_file in reade_file.get("posts", []):
-            print(title_file.get("title"))
+        with open(json_file , "r", encoding="utf-8") as file:
+            read_file = json.load(file)
+        return True
     except json.JSONDecodeError:
         print("خطا در خواندن فایل JSON")
+        return False
 
-print("عنوان های فایل\n")
-title_json()
+def title():
+    for title_file in read_file.get("posts", []):
+        print(title_file.get("title"))
+    
+
+def captions_file():
+    captions = 0
+    for caption in read_file["posts"]:
+        captions += len(caption["caption"])
+    print(captions)
+
+
+open_json()
+read_json()
+print(":عنوان های فایل\n")
+title()
+print("\nمجموع کپشن ها")
+captions_file()
+
+
+
+
