7e02e2a1db555afb4a2235a5743582cdefed564d
mohaddese <210817389+mohaddese-zamani@users.noreply.github.com>
2025-07-20 17:27:47 +0330

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -15,9 +15,11 @@ def practice1(jomleh):
 
 while True:
     try:
-        jomleh = input("\n\njomle ra vared konid\n ").split()
-        yekta, tartib_tekrar = practice1(sentence)
-        tedad_kalamat = sorted(tartib_tekrar.items(), key=lambda item: item[1], reverse=True)
+        jomleh = input("\njomle ra vared konid\n ").split()
+        yekta, tartib_tekrar = practice1(jomleh)
+        tabdil_list = list(tartib_tekrar.items())
+        list_moratab = sorted(key = lambda x: x[1], reverse=True)
+
 
         print(f"kalamat yekta = {yekta}")
         print(f"tartib tekrar kalamat =  {tedad_kalamat}")
