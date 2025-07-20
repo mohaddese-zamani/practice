929dba5b2de0600b75bd795d2fb3e9cd09e749d1
mohaddese <210817389+mohaddese-zamani@users.noreply.github.com>
2025-07-20 20:19:40 +0330

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -16,7 +16,7 @@ while True:
         yekta, tartib_tekrar = practice1(jomleh)
         tabdil_list = list(tartib_tekrar.items())
         list_moratab = sorted(tabdil_list, key = lambda x: x[1], reverse=True)
-        # tartib_tekrar.sort(key = lambda x: x[1], reverse=True)
+        
 
         
         print(f"kalamat yekta = {yekta}")
