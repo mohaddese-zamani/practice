457874f03165e5b09dae13192a58f9fbc6891da2
mohaddese <210817389+mohaddese-zamani@users.noreply.github.com>
2025-07-20 20:16:51 +0330

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -4,12 +4,9 @@ def practice1(jomleh):
 
     for i in jomleh:
         shomaresh = jomleh.count(i) 
-
         if shomaresh == 1:
             yekta.append(i)
-
         tartib_tekrar[i] = shomaresh
-        
     return yekta, tartib_tekrar
 
 
@@ -18,11 +15,17 @@ while True:
         jomleh = input("\njomle ra vared konid\n ").split()
         yekta, tartib_tekrar = practice1(jomleh)
         tabdil_list = list(tartib_tekrar.items())
-        list_moratab = sorted(key = lambda x: x[1], reverse=True)
+        list_moratab = sorted(tabdil_list, key = lambda x: x[1], reverse=True)
+        # tartib_tekrar.sort(key = lambda x: x[1], reverse=True)
 
-
+        
         print(f"kalamat yekta = {yekta}")
-        print(f"tartib tekrar kalamat =  {tedad_kalamat}")
-
+        print(f"tartib tekrar kalamat =  {tartib_tekrar}")
+        payan = input("آیا ادامه میدهید؟ بلی/خیر: ")
+        if payan == "بلی":
+            continue
+        else:
+            break
     except:
         pass
+
